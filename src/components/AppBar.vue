6b1de9b4a0cbf033c57dc568d2c44116f83801da
Timothé Frignac <timothe.frignac@yahoo.fr>
2025-02-02 16:53:32 +0100

    feat: disable map tab

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -73,9 +73,9 @@
 
   const links = [
     { id: 1, to: 'https://www.linkedin.com/company/trouver-une-fresque', text: 'Actualités', icon: 'mdi-newspaper' },
-    { id: 2, to: '/carte', text: 'Carte des fresques', icon: 'mdi-map' },
     { id: 3, to: '/apropos', text: 'About', icon: 'mdi-information' },
   ]
+  // { id: 2, to: '/carte', text: 'Carte des fresques', icon: 'mdi-map' },
 
   const theme = useTheme()
 
