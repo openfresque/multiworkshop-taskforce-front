9be043784e15fa30cd05989cccc107f8b0aec3a1
Thomas Bouvier <contact@thomas-bouvier.io>
2025-03-18 17:03:15 +0100

    feat: attempt at re-adding the map

--- src/pages/Carte.vue
+++ src/pages/Carte.vue
@@ -1,7 +1,125 @@
 <template>
-  <HelloWorld />
+  <v-container max-width="1200px">
+    <div
+      id="mapid"
+      style="
+        height: 80vh;
+        width: 90vw;
+        max-width: 100%;
+        max-height: 600px;
+        margin-bottom: 40px;
+        margin: auto;
+      "
+    ></div>
+  </v-container>
 </template>
 
 <script lang="ts" setup>
-  //
+  import { onMounted, onUnmounted, ref } from 'vue'
+  import { map, tileLayer, marker, Marker, Icon, LatLngTuple } from 'leaflet'
+  import 'leaflet/dist/leaflet.css'
+  import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
+  import { MarkerClusterGroup } from 'leaflet.markercluster'
+  import { State, Workshop } from '@/state/State'
+
+  type WorkshopCarte = {
+    nom: string
+    longitude: number
+    latitude: number
+    reservation: string
+    adresse: string | undefined
+  }
+
+  const mymap = ref<any>(null)
+
+  onMounted(async () => {
+    mymap.value = map('mapid', {
+      // Add any map options here
+    }).setView([46.505, 3], 6)
+
+    const allWorkshops = await State.current.allWorkshops()
+    const workshopsCarte = allWorkshops.workshopsDisponibles
+      .filter((workshop: Workshop) => workshop.online === false) // Exclude online workshops
+      .filter(workshop => !!workshop.longitude && workshop.latitude) // Exclude workshops without coordinates
+      .filter(
+        workshop =>
+          workshop.latitude >= -90 &&
+          workshop.latitude <= 90 &&
+          workshop.longitude >= -180 &&
+          workshop.longitude <= 180
+      ) // Exclude workshops with
+      .map<WorkshopCarte>(workshop => ({
+        nom: workshop.title,
+        longitude: workshop.longitude,
+        latitude: workshop.latitude,
+        reservation: workshop.source_link,
+        adresse: workshop.full_location,
+      }))
+
+    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(mymap.value)
+
+    const markers = creerPins(workshopsCarte)
+    mymap.value.addLayer(markers)
+  })
+
+  onUnmounted(() => {
+    if (mymap.value) {
+      mymap.value.remove()
+    }
+  })
+
+  function creerPins(lieux: WorkshopCarte[]) {
+    const markers = lieux.reduce((markers, lieu) => {
+      let reservation_str = ''
+      if (typeof lieu.reservation !== 'undefined') {
+        if (lieu.reservation.indexOf('http') === 0) {
+          reservation_str = `<a href="${lieu.reservation}">${lieu.reservation.slice(0, 35) + '...'}</a>`
+        }
+      } else {
+        reservation_str = lieu.reservation
+      }
+
+      const string_popup = `
+      <span style='font-size: 150%;'>${lieu.nom}</span>
+      <br>
+      <b>Adresse :</b> ${lieu.adresse || '-'}
+      <br>
+      <b>Réservation :</b> ${reservation_str || '-'}
+    `
+      const customIcon = new Icon({
+        iconUrl: `assets/images/png/marker-icon-2x.png`, // Path to your custom PNG icon
+        iconSize: [25, 41], // Size of the icon
+        iconAnchor: [12, 41], // Point of the icon which will correspond to marker's location
+        popupAnchor: [0 - 34], // Point from which the popup should open relative to the iconAnchor
+      })
+
+      const newMarker = marker([lieu.latitude, lieu.longitude] as LatLngTuple, {
+        icon: customIcon,
+      }).bindPopup(string_popup)
+      newMarker.on('click', function () {
+        // @ts-ignore
+        this.openPopup()
+      })
+
+      markers.push(newMarker)
+      return markers
+    }, [] as Marker[])
+
+    const markersGroup = new MarkerClusterGroup({
+      disableClusteringAtZoom: 9,
+      chunkedLoading: true,
+    })
+    markersGroup.addLayers(markers, true)
+    return markersGroup
+  }
 </script>
+
+<style scoped>
+  #mapid {
+    height: 180px;
+  }
+</style>
