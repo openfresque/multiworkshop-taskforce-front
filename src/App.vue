fc889986769266eb936a691451162380f33f7d05
Thomas Bouvier <contact@thomas-bouvier.io>
2025-03-19 16:25:04 +0100

    feat: respect user's theme preference

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,16 @@
 </template>
 
 <script lang="ts" setup>
+  import { useTheme } from 'vuetify'
   import { RemoteConfig } from './utils/RemoteConfig'
+
+  const theme = useTheme()
 
   onMounted(async () => {
     await RemoteConfig.INSTANCE.sync()
+
+    // Set theme based on user preference
+    const prefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+    theme.global.name.value = prefersDark ? 'dark' : 'light'
   })
 </script>
