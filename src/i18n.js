c5eb0561de9be7d34ef172f3fa6a9a2bee769641
Thomas Bouvier <contact@thomas-bouvier.io>
2025-04-10 10:51:21 +0200

    feat: translate workshops

--- src/i18n.js
+++ src/i18n.js
@@ -9,7 +9,7 @@ const messages = {
 
 const i18n = createI18n({
   legacy: false, // Use Composition API
-  locale: 'fr', // Set the default locale
+  locale: 'en', // Set the default locale
   messages,
 })
 
